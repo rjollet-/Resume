7fc77caf53155621355b7d6081286c1da8d97b59
rjollet- <renaudjollet@gmail.com>
2014-06-22 22:11:58 +0200

    cv

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('CV', { title: 'resume' });
+  res.render('cv', { title: 'resume' });
 });
 
 module.exports = router;
