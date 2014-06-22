7efefa87965766ed273ff96503dc7f1e52fb5706
rjollet- <renaudjollet@gmail.com>
2014-06-22 22:10:28 +0200

    /

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(process.cwd() + '/public'));
 
-app.use('/', CV);
+app.use('/', routes);
 app.use('/users', users);
 app.use('/cv', cv);
 app.use('/blogHome', blogHome);
